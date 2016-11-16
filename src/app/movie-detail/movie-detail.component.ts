08dff9e2b649f93afeeb403812fe753b14e3753a
Joshua Herkness <joshherkness@gmail.com>
2016-11-16 13:53:03 -0500

    Test +0000

--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -31,7 +31,7 @@ import * as io from 'socket.io-client';
         </select>
         <div class='movie-showtimes-wrapper'>
           <button *ngFor='let showtime of groupedShowtimes[selectedDay]' class='showtime' (click)='clickShowtime(showtime)'>
-            {{showtime.time | date:'h:mm a'}}
+            {{showtime.time| date:'h:mm a' : '+0000'}}
           </button>
         </div>
       </div>
